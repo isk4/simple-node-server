311ce3fa6af4ba76b5ab90789fe9680d98832bba
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2020-12-26 13:38:35 -0300

    Simple Node HTTP Server

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((request, response) => {
-  console.log(request.headers);
+  console.log(`Request for ${request.url} by method ${request.method}`);
 
-  response.statusCode = 200;
-  response.setHeader('Content-Type',  'text/html');
-  response.end('<html><body><h1>Wena wena</h1></body></html>');
+   if (request.method === 'GET') {
+    
+    let fileUrl = request.url === '/' ? 'index.html' : request.url;
+    
+    let filePath = path.resolve(`public/${fileUrl}`);
+    const fileExtension = path.extname(filePath);
+
+    if (fileExtension === '.html') {
+      fs.access(filePath, error => {
+        if (error) {
+          // Doesn't exist
+          response.statusCode = 404;
+          response.setHeader('Content-Type', 'text/html');
+          response.end(`<html><body><h1>404: ${fileUrl} not found.</h1></body></html>`);
+        } else {
+          // Exists
+          response.statusCode = 200;
+          response.setHeader('Content-Type', 'text/html');
+          fs.createReadStream(filePath).pipe(response);
+        }
+      });
+    } else {
+      response.statusCode = 404;
+      response.setHeader('Content-Type', 'text/html');
+      response.end(`<html><body><h1>404: ${fileUrl} is not an HTML file.</h1></body></html>`);
+    }
+
+  } else {
+    response.statusCode = 404;
+    response.setHeader('Content-Type', 'text/html');
+    response.end(`<html><body><h1>404: ${request.method} not supported.</h1></body></html>`);
+  }
+
 });
 
 server.listen(port, hostname, () => {
