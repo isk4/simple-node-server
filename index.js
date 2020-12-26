78d42179ee60888458547e36119075aa273b3d14
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2020-12-26 12:51:26 -0300

    Node HTTP example 1

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+  console.log(request.headers);
+
+  response.statusCode = 200;
+  response.setHeader('Content-Type',  'text/html');
+  response.end('<html><body><h1>Wena wena</h1></body></html>');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`)
+});
